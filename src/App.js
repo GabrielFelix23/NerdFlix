ac01a8b6a746f9a50e42b7ae1ecefaf6e381d379
GabrielFelix23 <srgfelix123@gmail.com>
2020-12-07 16:23:49 -0300

    projeto

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Route from './routes'
+import './style.css'
+import Routes from './routes'
+
+class App extends React.Component{
+
+    state = {
+       
+    }
+
+    render(){
+        return(
+            <div className="app">
+                <Routes/>
+            </div>
+        )
+    }
+}
+
+
+export default App
---
+++ src/pages/Erro/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Erro extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Página não encontrada!</h1>
+            </div>
+        )
+    }
+}
+
+export default Erro
